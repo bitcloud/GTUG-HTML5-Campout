24491cff345d9179682365da99d0cdc38942cbe4
mrmontezuma <greve@peppermind.de>
2010-08-14 19:43:33 +0200

    New menubar

--- colorwars.php
+++ colorwars.php
@@ -1,9 +1,15 @@
 <?php require 'header.php'; ?>
 	<body>
+	
 		<div id="topbar" style="margin-bottom: 0px;"> 
-			<div id="title"><div id="player"></div></div> 
-		</div> 		
+			<div id="title"><div id="matchName">ReLoCoring: Matchname</div></div>
+			<div id="leftnav"><a href="index.php">Men&uuml;</a></div>
+			<div id="rightnav"><a href="stats.php">Statistik</a></div>			
+		</div>	
+			
 		<div id="gmap"></div>
+		<div id="player"></div>
+		
 		<script>
 		jQuery(document).ready(function(){
 			jQuery('#player').html(gup('playername'));
