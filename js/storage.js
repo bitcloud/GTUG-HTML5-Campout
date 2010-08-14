e4779a8184e4918ab318b1339291f8e43b99605e
christine <ck@.(none)>
2010-08-14 12:09:19 +0200

    initial version of the readings methods interface

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,24 @@
-/** contains methods for reading and writing gps data from and to the local storage **/
+/** 
+ * contains methods for reading and writing gps coordinates from and to the local storage. 
+ * A coordinate contains a timestamp, the latitude and the longitude 
+ * 
+ * 
+ * **/
+
+/**
+ * Returns a list of coordinates as JSON objects, which timestamps are contained in the interval
+ * between 'fromTime' and 'toTime'.
+ * parameter from:
+ * parameter to:
+ */
+function readCoordinates(var from, var to) {
+	
+}
+
+/**
+ * Returns a single coordinate, the one next after given timestamp as JSON Object
+ * parameter time:
+ */
+function readNextCoordinate (var time){
+	
+}
