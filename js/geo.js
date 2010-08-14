5fa068d0fb781b453fb411b4fd2fe9794010450f
Elmar Schraml <elmar.schraml@gmail.com>
2010-08-14 15:33:30 +0200

    added function for peridocially storing position, scaffolding for comparing locations

--- js/geo.js
+++ js/geo.js
@@ -2,16 +2,52 @@
  * Geo functions to be called by client html page
  */
 
-/*
- * determine current position, and write it to local storage
- */
-function storeCurrentLocation() {
+//periodically stores current location, to be called on document.ready
+function periodicallyUpdateLocation() {
+	var interval = 1000 * 60; //one minute
+	setInterval(storeCurrentLocation,interval);
+}
+
+
+// determine current position, and write it to local storage
+function storeCurrentLocation( {
+	var thresholdInMeters = 30;
 
 	navigator.geolocation.getCurrentPosition(function(position) {
-	  writeCoordinate(position.coords.latitude, position.coords.longitude);
+	   var curLat = position.coords.latitude;
+	   var curLong = position.coords.longitude;
+	   if (distanceFromLastLocation(curLat,curLong > thresholdInMeters) ) {
+	      writeCoordinate(lat,long);		
+	   }
+
 	}, function(error) {
 	    alert('could not get position');
 	});
 	
+}
+
+//gets diff in meters between current location, passe as param, and last known location in localstorage
+function distanceFromLastLocation(curLat, curLong) {
+	//TODO: implement: get last from storage, calculateDistance()
+	//TODO: what if we have several small changes of loc < threshold, that together make up a store-worthy change > threshold?
+	return 100;
+}
+
+//monkey patching for calculateDistance function
+Number.prototype.toRad = function() {
+  return this * Math.PI / 180;
+}
+
+//copied off the web, returns distance in metres
+function calculateDistance(lat1, lon1, lat2, lon2) {
+  var R = 6371; // km
+  var dLat = (lat2 - lat1).toRad();
+  var dLon = (lon2 - lon1).toRad(); 
+  var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+          Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) * 
+          Math.sin(dLon / 2) * Math.sin(dLon / 2); 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)); 
+  var d = R * c;
+  return d*1000; //modified to return m rathern than km
 }
 
