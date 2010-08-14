d43f7dabe8aeec04b2f36a573b2f2a52e02dd260
mrmontezuma <greve@peppermind.de>
2010-08-14 20:06:10 +0200

    Mockup display of players

--- colorwars.php
+++ colorwars.php
@@ -5,10 +5,27 @@
 			<div id="title"><div id="matchName">ReLoCoring: Matchname</div></div>
 			<div id="leftnav"><a href="index.php">Men&uuml;</a></div>
 			<div id="rightnav"><a href="stats.php">Statistik</a></div>			
-		</div>	
-			
+		</div> 	
+		
 		<div id="gmap"></div>
-		<div id="player"></div>
+		
+		<div id="playerMe" class="player">
+			<div class="playerBg bgMe"></div>
+			<div class="playerFg">
+				<img src="iwebkit/avatar-nils.jpg" />
+				<div id="player">Niels</div>
+				<span>750 qm</span>
+			</div>
+		</div>
+		
+		<div id="playerEnemy" class="player">
+			<div class="playerBg bgEnemy"></div>
+			<div class="playerFg">
+				<img src="iwebkit/avatar-jan.jpg" />
+				<div>Jan</div>
+				<span>637 qm</span>
+			</div>
+		</div>
 		
 		<script>
 		jQuery(document).ready(function(){
